939487425c48dbef1064ac4c4da96b03a1879546
LizManchego <karinamanchego0416@gmail.com>
2024-05-29 11:04:51 -0600

    Update Actividad Didactica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,15 +7,15 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/arrastrar.svg")
+            img(src="@/assets/template/Falso_Verdadero.svg")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Presupuestando materias primas
-        p.mb-4 Diferenciar los diferentes  #[i softwares ] que permiten la optimización de las materias primas.
+          h2 Patronaje digital de prendas de vestir
+        p.mb-4 Reconocimento de los diferentes  #[i softwares ] que permiten la optimización de las materias primas.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Completar espacios
+              p.fw-bold.mb-0 Falso o Verdadero
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
